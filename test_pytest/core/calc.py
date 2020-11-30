6ae720fa7bd1cc2cb3abd4e771334d0d1c95badf
tongtong <xutongtong@ruijie.com.cn>
2020-11-30 17:02:20 +0800

    update 测试用例集

---
+++ test_pytest/core/calc.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/11/30 11:02
+# @Author  : tongtong
+# @File    : calc.py
+# @Software: Pycharm
+
+# 计算器乘除法的类
+class Calc:
+
+    def div(self, a, b):
+        return a / b
+
+    def mul(self, a, b):
+        return a * b
+
+
+if __name__ == '__main__':
+    calc = Calc()
+    print(calc.div('a', 0))
